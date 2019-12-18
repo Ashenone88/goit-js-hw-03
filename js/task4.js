b0b6840dfab62d06580488214c6003170c96cbf6
Ashenone88 <yarovantv@gmail.com>
2019-12-18 22:15:15 +0200

    init hw3

---
+++ js/task4.js
@@ -0,0 +1,29 @@
+"use strict";
+const countTotalSalary = function(employees) {
+  let totalSalary = 0;
+  for (let value of Object.values(employees)) {
+    totalSalary += value;
+  }
+  return totalSalary;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности реализации.
+ */
+console.log(countTotalSalary({})); // 0
+
+console.log(
+  countTotalSalary({
+    mango: 100,
+    poly: 150,
+    alfred: 80
+  })
+); // 330
+
+console.log(
+  countTotalSalary({
+    kiwi: 200,
+    lux: 50,
+    chelsy: 150
+  })
+); // 400
